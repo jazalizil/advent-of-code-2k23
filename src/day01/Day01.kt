369fc94a6d88d5c9ae4b34bcc3a8630e4403a3e1
Jalil <dabbechjalil@yahoo.fr>
2023-12-12 14:27:21 +0100

    chore: prettier day01

--- src/day01/Day01.kt
+++ src/day01/Day01.kt
@@ -29,15 +29,15 @@ fun main() {
     fun part2(input: List<String>): Int {
         return input.sumOf {
             it.replace(
-                    "one" to "1",
-                    "two" to "2",
-                    "three" to "3",
-                    "four" to "4",
-                    "five" to "5",
-                    "six" to "6",
-                    "seven" to "7",
-                    "eight" to "8",
-                    "nine" to "9"
+                "one" to "1",
+                "two" to "2",
+                "three" to "3",
+                "four" to "4",
+                "five" to "5",
+                "six" to "6",
+                "seven" to "7",
+                "eight" to "8",
+                "nine" to "9"
             ).toDigits()
         }
     }
