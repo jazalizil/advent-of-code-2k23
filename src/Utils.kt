891e85e58a656ec2b7e58ce35e93fc3283b79b03
Jalil <dabbechjalil@yahoo.fr>
2023-12-12 14:26:40 +0100

    chore: day05
    fail

--- src/Utils.kt
+++ src/Utils.kt
@@ -1,3 +1,6 @@
+import kotlinx.coroutines.async
+import kotlinx.coroutines.awaitAll
+import kotlinx.coroutines.coroutineScope
 import java.math.BigInteger
 import java.security.MessageDigest
 import kotlin.io.path.Path
@@ -19,3 +22,11 @@ fun String.md5() = BigInteger(1, MessageDigest.getInstance("MD5").digest(toByteA
  * The cleaner shorthand for printing output.
  */
 fun Any?.println() = println(this)
+
+
+/**
+ * Async list iteration
+ */
+suspend fun <T, R> List<T>.mapAsync(
+    mapper: suspend (T) -> R
+): List<R> = coroutineScope { map { async { mapper(it) } }.awaitAll() }
--- src/day05/Day05.kt
+++ src/day05/Day05.kt
@@ -1,51 +1,113 @@
 package day05
 
+import kotlinx.coroutines.flow.*
+import mapAsync
 import println
 import readInput
 
-data class Mapper(
-    val destinationRange: IntRange,
-    val sourceRange: IntRange
-)
+class Mapper(
+    val range: UIntRange,
+    val startValue: UInt,
+) {
+    fun getResult(fromValue: UInt): UInt {
+        return this.startValue + fromValue - this.range.first
+    }
+}
 
-fun String.getSeeds(): Set<Int> {
+fun String.getSeeds(): List<UInt> {
     val split = this.split(": ", " ")
-    return split.takeLast(split.size-1).mapTo(HashSet()) { it.toInt() }
+    return split.takeLast(split.size-1).map { it.toUInt() }
+}
+
+fun String.getSeedRanges(): List<UIntRange> {
+    val split = this.split(": ", " ")
+    return split.takeLast(split.size-1).chunked(2).map { it[0].toUInt()..<(it[0].toUInt())+it[1].toUInt() }
 }
 
 fun String.getMapper(): Mapper {
-    val data = this.split(" ").map { it.trim().toInt() }
+    val data = this.split(" ").map { it.trim().toUInt() }
     return Mapper(
-        destinationRange = data.first()..data.first() + data.last(),
-        sourceRange = data[1]..data[1] + data.last()
+        range = data[1]..data[1] + data.last(),
+        startValue = data[0]
     )
 }
 
-fun main() {
-    fun part1(input: List<String>): Int {
-        var base = input.first().getSeeds();
-        val seedToLocation = mutableMapOf<Int, Int>();
-        for (i in 1..99) {
-            seedToLocation[i] = i
+fun List<String>.mapSeedInRangeRecursive(from: UInt): UInt {
+    if (this.isEmpty()) {
+        return from
+    }
+    if (this.first().isEmpty()||this.first().contains(":")) {
+        return this.takeLast(this.size-1).mapSeedInRangeRecursive(from)
+    }
+    val mapper = this.first().getMapper()
+    if (from !in mapper.range) {
+        return this.takeLast(this.size-1).mapSeedInRangeRecursive(from)
+    }
+    val nextTransformIndex = this.indexOfFirst { it.contains(":") }
+    if (nextTransformIndex > 0)
+        return this.takeLast(this.size-nextTransformIndex).mapSeedInRangeRecursive(mapper.getResult(from))
+    return mapper.getResult(from)
+}
+
+val UIntRange.min
+    get() = minOf(start, endInclusive)
+
+val UIntRange.max
+    get() = maxOf(start, endInclusive)
+
+val UIntRange.size
+    get() = max - min
+infix fun UIntRange.intersect(other: UIntRange) : UIntRange? =
+    if (min <= other.max && other.min <= max)
+        maxOf(min, other.min).rangeTo(minOf(max, other.max))
+    else
+        null
+
+fun List<String>.mapSeedInRangeIterative(seeds: UIntRange) : UInt {
+    var finals = listOf<UInt>()
+    var lines = this
+    while (lines.isNotEmpty()) {
+        if (lines.first().isEmpty() || lines.first().contains(":")) {
+            lines = lines.takeLast(lines.size - 1)
+            continue;
         }
-        for (line in 1..<input.size) {
-            while (input[line].isNotEmpty() && !input[line].contains(":")) {
-                val mapper = input[line].getMapper()
-                for (key in mapper.sourceRange) {
-                    val index = seedToLocation.replace(key, mapper.destinationRange.elementAt(key))
-                    index
-                }
-            }
+        val mapper = lines.first().getMapper()
+        val nextIndex = lines.indexOfFirst { it.contains(":") }
+        val inter = seeds.intersect(mapper.range)
+        if (inter == null) {
+            lines = lines.takeLast(lines.size - 1)
+            continue
         }
-        return input.size
+        finals = inter.mapIndexed { index, it -> mapper.getResult(if (index in finals.indices) finals[index] else it) }
+        if (nextIndex < 0) {
+            break;
+        }
+        lines = lines.takeLast(lines.size - nextIndex)
+    }
+    return finals.min()
+}
+
+
+
+suspend fun main() {
+    fun part1(input: List<String>): UInt {
+        val seeds = input.first().getSeeds()
+        return seeds.minOf { input.takeLast(input.size-1).mapSeedInRangeRecursive(it) }
     }
 
-    fun part2(input: List<String>): Int {
-        return input.size
+    suspend fun part2(input: List<String>): UInt {
+        val seeds = input.first().getSeedRanges()
+        return seeds.asFlow().toList().mapAsync { range ->
+            "processing ${range.count()} values in range $range".println()
+            val final = range.minOf {  input.takeLast(input.size - 1).mapSeedInRangeRecursive(it) }
+            "done process range $range with $final".println()
+            final
+        }.min()
     }
 
     val testInput = readInput("day05/Day05_test")
-    check(part1(testInput) == 35)
+    check(part1(testInput) == 35U)
+    check(part2(testInput) == 46U)
 
     val input = readInput("day05/Day05")
     part1(input).println()
